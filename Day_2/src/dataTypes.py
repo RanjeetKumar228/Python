db78f91645a0b46e17091e1164e6bc0c403efc1d
Ranjeet Kumar <ranjeet228k@gmail.com>
2025-11-08 07:20:33 +0530

    Introduction to Data Types

--- Day_2/src/dataTypes.py
+++ Day_2/src/dataTypes.py
@@ -1,1 +1,31 @@
-# Data Types
+                # Data Types
+
+# int    -->    Integer     -->  -inf, ...-1, 0, 1, ...inf
+# 
+# float  -->    Decimal     -->  1.2, 4.5, etc..
+# 
+# str    -->    Character   -->  "a", 'r', "hello ranjeet", "#", "  ", "@"
+
+
+
+                # Variable
+
+
+ranjeet = 30
+sunny = 20
+raju = 29
+print(ranjeet)
+print(ranjeet+sunny)
+
+
+x = 1
+y = 2
+z = 3
+print(x+y)
+
+a, b, c = 11, 12, 13
+print(a,b,c)
+
+a = b
+b = c
+print("a : ",a , " b : ", b )
