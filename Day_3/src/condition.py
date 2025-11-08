648c90e4b255e1140c3c8273d43fd7e71e593255
Ranjeet Kumar <ranjeet228k@gmail.com>
2025-11-08 07:52:53 +0530

    Internal file

---
+++ Day_3/src/condition.py
@@ -0,0 +1,44 @@
+                # if else
+
+ticket_price = 90
+wallet_size = 100
+
+# print(ticket_price)
+
+if ticket_price <= wallet_size:
+    print("Hurray, i can go to this movie!")
+else:
+    print("Sad, i don't have enough money")
+
+
+
+# Syntax
+
+# if(condition):
+#     to do
+# else:
+#     to do
+
+
+#......................................................
+
+# 90+ A+
+# 80+ A
+# 70+ B+
+# 60+ B
+# 60- fail
+#
+# if else elif
+
+marks = 50
+
+if (marks >= 90):
+    print("A+")
+elif (marks >= 80):
+    print("A")
+elif (marks >= 70):
+    print("B+")
+elif(marks >= 60):
+    print("B")
+else:
+    print("Fail")
