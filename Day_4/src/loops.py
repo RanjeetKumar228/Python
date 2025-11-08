0fb49689b4d3ef5ec7004bbc28e76b3cf5200129
Ranjeet Kumar <ranjeet228k@gmail.com>
2025-11-08 18:54:18 +0530

    loops

---
+++ Day_4/src/loops.py
@@ -0,0 +1,30 @@
+# for loop
+
+# for range(start, end);
+# by default start = 0, range(1,10)
+
+# for i in range(100):
+#      print(i)
+
+#...................................................
+
+# print multiple of 10
+
+#    n % 10 --> 0
+
+num = 90
+
+if (num%10) == 0:
+    print(num)
+else:
+    print(" ")
+
+
+# ..............................................
+
+
+for i in range(1,10):
+    if(i%3) == 0:
+        print(i)
+    else:
+        print("")
